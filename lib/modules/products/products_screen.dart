6ca9636ab706c5fb642dbea825e52f1027f4b185
Mahmoud <mahmoud.harooney.tooling@gmail.com>
2021-05-09 21:33:38 +0200

    Products Grid created

--- lib/modules/products/products_screen.dart
+++ lib/modules/products/products_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_app/models/home_model.dart';
 import 'package:shop_app/shared/cubit.dart';
 import 'package:shop_app/shared/states.dart';
+import 'package:shop_app/styles/colors.dart';
 
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({Key key}) : super(key: key);
@@ -17,7 +18,7 @@ class ProductsScreen extends StatelessWidget {
         return ConditionalBuilder(
           condition: AppCubit.get(context).homeModel != null,
           builder: (context) =>
-              productsBuilder(AppCubit.get(context).homeModel, context),
+              builderWidget(AppCubit.get(context).homeModel, context),
           fallback: (context) => Center(
             child: CircularProgressIndicator(),
           ),
@@ -26,31 +27,122 @@ class ProductsScreen extends StatelessWidget {
     );
   }
 
-  Widget productsBuilder(HomeModel model, context) => Column(
-        children: [
-          CarouselSlider(
-            items: model.data.banners
-                .map(
-                  (e) => Image(
-                    image: NetworkImage('${e.image}'),
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                )
-                .toList(),
-            options: CarouselOptions(
-              height: MediaQuery.of(context).size.height * 0.3,
-              initialPage: 0,
-              viewportFraction: 1.0,
-              enableInfiniteScroll: true,
-              reverse: false,
-              autoPlay: true,
-              autoPlayInterval: Duration(seconds: 3),
-              autoPlayAnimationDuration: Duration(seconds: 1),
-              autoPlayCurve:Curves.fastOutSlowIn,
-              scrollDirection: Axis.horizontal,
+  Widget builderWidget(HomeModel model, context) => SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            CarouselSlider(
+              items: model.data.banners
+                  .map(
+                    (e) => Image(
+                      image: NetworkImage('${e.image}'),
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                  .toList(),
+              options: CarouselOptions(
+                height: MediaQuery.of(context).size.height * 0.3,
+                initialPage: 0,
+                viewportFraction: 1.0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlay: true,
+                autoPlayInterval: Duration(seconds: 3),
+                autoPlayAnimationDuration: Duration(seconds: 1),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                scrollDirection: Axis.horizontal,
+              ),
             ),
-          )
-        ],
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              color: Colors.grey[300],
+              child: GridView.count(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                mainAxisSpacing: 1.0,
+                crossAxisSpacing: 1.0,
+                childAspectRatio: 1 / 1.58,
+                children: List.generate(
+                  model.data.products.length,
+                  (index) => buildGridProduct(model.data.products[index]),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget buildGridProduct(ProductModel model) => Container(
+        color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              // alignment: AlignmentDirectional.bottomStart,
+              children: [
+                Image(
+                  image: NetworkImage(model.image),
+                  width: double.infinity,
+                  height: 200.0,
+                  // fit: BoxFit.cover,
+                ),
+                if (model.discount != 0)
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12.0),
+                    child: Image.asset(
+                      'assets/images/discount.png',
+                      width: 30.0,
+                    ),
+                  ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    model.name,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      height: 1.3,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '${model.price.round()}',
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          color: defaultColor,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5.0,
+                      ),
+                      if (model.discount != 0)
+                        Text(
+                          '${model.oldPrice.round()}',
+                          style: TextStyle(
+                            fontSize: 10.0,
+                            color: Colors.grey,
+                            decoration: TextDecoration.lineThrough,
+                            decorationColor: Colors.red,
+                            decorationThickness: 2.0,
+                          ),
+                        ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       );
 }
